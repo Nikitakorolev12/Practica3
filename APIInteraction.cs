4b8e11325d32bf50a8675b96b4fcbfb45c2b6e32
vr1men <svrmen12@gmail.com>
2025-04-14 07:37:56 +0300

    Добавьте файлы проекта.

---
+++ APIInteraction.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using Word = Microsoft.Office.Interop.Word;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Practica3
+{
+    public class APIInteraction
+    {
+        private string fullname;
+        private bool ContainsExtraChars(string input)
+        {
+            return !Regex.IsMatch(input, @"^[А-Яа-я\s]+$");
+        }
+        public string GetFullName()
+        {
+            string URL = "http://localhost:4444/TransferSimulator/fullName";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "GET";
+            request.Proxy.Credentials = new NetworkCredential("student", "student");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string text = reader.ReadToEnd();
+            JObject jobject = JObject.Parse(text);
+            string value = (string)jobject["value"];
+            fullname = value;
+            return fullname;
+        }
+        public string FillDocument()
+        {
+            string result = "";
+            if (fullname == null)
+            {
+                MessageBox.Show("<<!Данные не были получены!>>");
+                return result;
+            }
+            bool isValidFullname = ContainsExtraChars(fullname);
+            result = isValidFullname ? "ФИО содержит запрещенные символы" : "ФИО не содержит запрещенные символы";
+            string[] rowData = { $"Введены данные \n{fullname}", result, "Успешно" };
+            AddToWordTable(rowData);
+            return result;
+        }
+        public void AddToWordTable(string[] rowData)
+        {
+            var openFileDlg = new System.Windows.Forms.OpenFileDialog();
+            string filePath = "";
+            if (openFileDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                filePath = openFileDlg.FileName;
+            else return;
+            Word.Application wordApp = new Word.Application();
+            Word.Document doc = null;
+
+            try
+            {
+                doc = wordApp.Documents.Open(filePath);
+                wordApp.Visible = false;
+                Word.Table table = doc.Tables[1];
+                Word.Row row = table.Rows.Add();
+                for (int i = 0; i < rowData.Length; i++)
+                {
+                    row.Cells[i + 1].Range.Text = rowData[i];
+                }
+
+                doc.Save();
+
+                MessageBox.Show("");
+            }
+            catch
+            {
+                MessageBox.Show("");
+            } 
+            finally
+            {
+                if (doc != null)
+                {
+                    doc.Close(Word.WdSaveOptions.wdSaveChanges);
+                }
+                wordApp.Quit(Word.WdSaveOptions.wdSaveChanges);
+            }
+            
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private APIInteraction appinteraction;
+        public MainWindow()
+        {
+            InitializeComponent();
+            appinteraction = new APIInteraction();
+        }
+
+        private void ButtonGetFullName_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlockFullName.Text = appinteraction.GetFullName();
+        }
+
+        private void ButtonSendResult_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlockResult.Text = appinteraction.FillDocument();
+        }
+    }
+}
